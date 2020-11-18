39038d6e70c17538813b1daf8584a7e1617e5b9f
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2020-11-18 14:23:26 -0500

    commit tree a00c4d3614d44307e2cf094bc2e336f42c258c3c

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
+require 'pry'
+
 class Pokemon
+
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(name:, type:, id:, db:)
+        @name = name
+        @type = type
+        @id = id
+        @db = db  
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
+    end
+
+    def self.find(id_number, db)
+        pokemon_info = db.execute("SELECT * FROM pokemon WHERE id = ?;", id_number)
+        self.new(name: pokemon_info.flatten[1], type: pokemon_info.flatten[2], id: pokemon_info.flatten[0], db: db)
+    end
+
 end
